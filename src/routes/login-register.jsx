5775cd75a8cbe34642f65a2f301d1b6d5c1ebcf1
Manav Misra <manavm@visionwebsoft.com>
2023-02-08 13:21:32 -0600

    Route to Login/Register page

---
+++ src/routes/login-register.jsx
@@ -0,0 +1,83 @@
+import { useState } from "react";
+import Button from "../components/button";
+import Error from "../components/error";
+import InputText from "../components/form/input-text";
+import apiService from "../services/api";
+
+export default function LoginRegister() {
+  const [isRegistering, setIsRegistering] = useState(true);
+  const [error, setError] = useState(null);
+
+  const handleError = (error) => {
+    if (error.response) {
+      error.response.json().then((json) => {
+        setError(json);
+      });
+    } else {
+      setError({ message: error.message });
+    }
+  };
+
+  return (
+    <main className="flex flex-col gap-y-4">
+      <form
+        className="mt-4 flex flex-col items-center gap-y-4"
+        onSubmit={(e) => {
+          e.preventDefault();
+
+          const submittedUser = {
+            username: e.target.username.value,
+            password: e.target.password.value,
+          };
+
+          if (isRegistering) {
+            if (e.target.password.value !== e.target.confirmPassword.value) {
+              setError({ message: "Passwords do not match." });
+              return;
+            }
+          }
+
+          apiService
+            .loginOrRegister(submittedUser, isRegistering)
+            .then((response) => {
+              // TODO: Avoid using localStorage for tokens!
+              if (response.token) {
+                localStorage.setItem("token", response.token);
+              }
+            })
+            .catch((error) => {
+              handleError(error);
+            });
+        }}
+        onFocus={() => {
+          setError(null);
+        }}
+      >
+        <InputText label="Username" id="username" />
+        <InputText label="Password" id="password" type="password" />
+
+        {isRegistering && (
+          <InputText
+            label="Confirm Password"
+            id="confirmPassword"
+            type="password"
+          />
+        )}
+
+        <Button type="submit" text={isRegistering ? "Sign Up" : "Login"} />
+
+        {/* Conditional rendering: IF error is updated to something truthy (not null)... */}
+        {error && <Error message={error.message} />}
+      </form>
+
+      <button
+        className="text-center"
+        onClick={() => {
+          setIsRegistering((prev) => !prev);
+        }}
+      >
+        {isRegistering ? "Already have an account?" : "Don't have an account?"}
+      </button>
+    </main>
+  );
+}
