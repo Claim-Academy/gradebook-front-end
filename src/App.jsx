36eb2ffcff48f7b4d17cb65b94eb5f10676ede81
Manav Misra <manavm@visionwebsoft.com>
2023-02-09 13:01:27 -0600

    Use useEffect to load the the students

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import LoginRegister from "./routes/login-register";
+import Students from "./routes/students";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <LoginRegister />,
+  },
+
+  // TODO: Protect this route (check for a jwt)
+  {
+    path: "/students",
+    element: <Students />,
   },
 ]);
 
---
+++ src/routes/students.jsx
@@ -0,0 +1,22 @@
+import { useEffect, useState } from "react";
+import apiService from "../services/api";
+
+export default function Students() {
+  const [students, setStudents] = useState([]);
+
+  // 'useEffect' is a React hook that runs EVERY TIME the component is rendered
+  useEffect(
+    () => {
+      apiService.getAllStudents().then((students) => setStudents(students));
+    },
+
+    // DEPENDENCY ARRAY: If any of these values change, the useEffect hook will run again
+    []
+  );
+
+  return (
+    <div>
+      <h1>Students</h1>
+    </div>
+  );
+}
