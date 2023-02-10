647d30d08653c5a28f2de551f5a0196d84b41959
Manav Misra <manavm@visionwebsoft.com>
2023-02-10 13:57:21 -0600

    Render the add student form

--- src/routes/students.jsx
+++ src/routes/students.jsx
@@ -1,5 +1,7 @@
 import { useLoaderData } from "react-router-dom";
+import InputText from "../components/form/input-text";
 import StudentSections from "../components/student-sections/student-sections";
+import Button from "../components/button";
 
 export default function Students() {
   const students = useLoaderData();
@@ -7,6 +9,10 @@ export default function Students() {
   return (
     <div>
       <h1 className="text-center">Students</h1>
+      <form className="flex flex-col items-center gap-y-4">
+        <InputText label="Add Student" id="addStudent" />
+        <Button text="Add Student" />
+      </form>
       <StudentSections students={students} />
     </div>
   );
